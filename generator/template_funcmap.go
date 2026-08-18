02fe5c6bb97fe1bf178aa3f51f176d083ed9334d
Frederic BIDON <fredbi@yahoo.com>
2026-08-18 19:40:51 +0200

    refact(generator): replaced funcmap which typed input by method exposed as field
    Signed-off-by: Frederic BIDON <fredbi@yahoo.com>

--- generator/template_funcmap.go
+++ generator/template_funcmap.go
@@ -4,8 +4,117 @@
 package generator
 
 import (
+	"errors"
 	"fmt"
+	"path/filepath"
+	"text/template"
+
+	"github.com/go-swagger/go-swagger/generator/internal/language"
 )
+
+// TODO: move this down to an internal package.
+// Current problem: functions that depends on types
+
+var docFormat = map[string]string{
+	binary: "binary (byte stream)",
+	b64:    "byte (base64 string)",
+}
+
+var errInternal = errors.New("internal error detected in templates")
+var errInvalidPlugin = errors.New("invalid template plugin")
+
+// DefaultFuncMap yields a map with default functions for use in the templates.
+// These are available in every template.
+func DefaultFuncMap(lang *language.Options) template.FuncMap {
+	f := golangfuncs.FuncMap(lang.Mangler)
+	pascalize, ok := f["pascalize"].(func(string) string)
+	if !ok {
+		panic("internal error: expected pascalize to be func(string) string")
+	}
+
+	// Language-specific entries that depend on *LanguageOpts.
+	f["varname"] = lang.MangleVarName
+	f["snakize"] = lang.MangleFileName
+	f["toPackagePath"] = func(name string) string {
+		return filepath.FromSlash(lang.ManglePackagePath(name, ""))
+	}
+	f["toPackage"] = func(name string) string {
+		return lang.ManglePackagePath(name, "")
+	}
+	f["toPackageName"] = func(name string) string {
+		return lang.ManglePackageName(name, "")
+	}
+	f["arrayInitializer"] = lang.ArrayInitializer
+	f["imports"] = lang.Imports
+
+	// Generator-type-dependent entries.
+	f["paramDocType"] = func(param GenParameter) string {
+		return resolvedDocType(param.SwaggerType, param.SwaggerFormat, param.Child)
+	}
+	f["headerDocType"] = func(header GenHeader) string {
+		return resolvedDocType(header.SwaggerType, header.SwaggerFormat, header.Child)
+	}
+	f["schemaDocType"] = func(in any) string {
+		switch schema := in.(type) {
+		case GenSchema:
+			return resolvedDocSchemaType(schema.SwaggerType, schema.SwaggerFormat, schema.Items)
+		case *GenSchema:
+			if schema == nil {
+				return ""
+			}
+			return resolvedDocSchemaType(schema.SwaggerType, schema.SwaggerFormat, schema.Items)
+		case GenDefinition:
+			return resolvedDocSchemaType(schema.SwaggerType, schema.SwaggerFormat, schema.Items)
+		case *GenDefinition:
+			if schema == nil {
+				return ""
+			}
+			return resolvedDocSchemaType(schema.SwaggerType, schema.SwaggerFormat, schema.Items)
+		default:
+			panic("dev error: schemaDocType should be called with GenSchema or GenDefinition")
+		}
+	}
+	f["schemaDocMapType"] = func(schema GenSchema) string {
+		return resolvedDocElemType("object", schema.SwaggerFormat, &schema.resolvedType)
+	}
+	f["docCollectionFormat"] = resolvedDocCollectionFormat
+	// f["path"] = errorPath // deprecated
+
+	// CLI command helpers that depend on generator types.
+	f["cmdName"] = func(in any) (string, error) {
+		op, isOperation := in.(GenOperation)
+		if !isOperation {
+			ptr, ok := in.(*GenOperation)
+			if !ok {
+				return "", fmt.Errorf("cmdName should be called on a GenOperation, but got: %T", in)
+			}
+			op = *ptr
+		}
+		name := "Operation" + pascalize(op.Package) + pascalize(op.Name) + "Cmd"
+
+		return name, nil
+	}
+	f["cmdGroupName"] = func(in any) (string, error) {
+		opGroup, ok := in.(GenOperationGroup)
+		if !ok {
+			return "", fmt.Errorf("cmdGroupName should be called on a GenOperationGroup, but got: %T", in)
+		}
+		name := "GroupOfOperations" + pascalize(opGroup.Name) + "Cmd"
+
+		return name, nil
+	}
+
+	// assert is used to inject into templates and check for inconsistent/invalid data.
+	f["assert"] = func(msg string, assertion bool) (string, error) {
+		if !assertion {
+			return "", fmt.Errorf("%v: %w", msg, errInternal)
+		}
+
+		return "", nil
+	}
+
+	return f
+}
 
 func resolvedDocCollectionFormat(cf string, child *GenItems) string {
 	if child == nil {
@@ -89,77 +198,4 @@ func resolvedDocElemType(tn, ft string, schema *resolvedType) string {
 	}
 
 	return tn
-}
-
-func errorPath(in any) (string, error) {
-	var pth string
-	rooted := func(schema GenSchema) string {
-		if schema.WantsRootedErrorPath && schema.Path == "" && (schema.IsArray || schema.IsMap) {
-			return `"[` + schema.Name + `]"`
-		}
-
-		return schema.Path
-	}
-
-	switch schema := in.(type) {
-	case GenSchema:
-		pth = rooted(schema)
-	case *GenSchema:
-		if schema == nil {
-			break
-		}
-		pth = rooted(*schema)
-	case GenDefinition:
-		pth = rooted(schema.GenSchema)
-	case *GenDefinition:
-		if schema == nil {
-			break
-		}
-		pth = rooted(schema.GenSchema)
-	case GenParameter:
-		pth = schema.Path
-
-	// unchanged Path if called with other types
-	case *GenParameter:
-		if schema == nil {
-			break
-		}
-		pth = schema.Path
-	case GenResponse:
-		pth = schema.Path
-	case *GenResponse:
-		if schema == nil {
-			break
-		}
-		pth = schema.Path
-	case GenOperation:
-		pth = schema.Path
-	case *GenOperation:
-		if schema == nil {
-			break
-		}
-		pth = schema.Path
-	case GenItems:
-		pth = schema.Path
-	case *GenItems:
-		if schema == nil {
-			break
-		}
-		pth = schema.Path
-	case GenHeader:
-		pth = schema.Path
-	case *GenHeader:
-		if schema == nil {
-			break
-		}
-		pth = schema.Path
-	default:
-		return "", fmt.Errorf("errorPath should be called with GenSchema or GenDefinition, but got %T", schema)
-	}
-
-	if pth == "" {
-		return `""`, nil
-	}
-
-	return pth, nil
 }
--- generator/template_repo.go
+++ generator/template_repo.go
@@ -5,14 +5,8 @@ package generator
 
 import (
 	"embed"
-	"errors"
 	"fmt"
 	"io/fs"
-	"path/filepath"
-	"text/template"
-
-	golangfuncs "github.com/go-swagger/go-swagger/generator/internal/funcmaps/golang"
-	"github.com/go-swagger/go-swagger/generator/internal/language"
 )
 
 // templateAssets holds the templates shipped with the generator.
@@ -60,107 +54,4 @@ func contribTemplates(name string) (fs.FS, error) {
 	}
 
 	return rooted, nil
-}
-
-var (
-	errInternal      = errors.New("internal error detected in templates")
-	errInvalidPlugin = errors.New("invalid template plugin")
-)
-
-var docFormat = map[string]string{
-	binary: "binary (byte stream)",
-	b64:    "byte (base64 string)",
-}
-
-// DefaultFuncMap yields a map with default functions for use in the templates.
-// These are available in every template.
-func DefaultFuncMap(lang *language.Options) template.FuncMap {
-	f := golangfuncs.FuncMap(lang.Mangler)
-	pascalize, ok := f["pascalize"].(func(string) string)
-	if !ok {
-		panic("internal error: expected pascalize to be func(string) string")
-	}
-
-	// Language-specific entries that depend on *LanguageOpts.
-	f["varname"] = lang.MangleVarName
-	f["snakize"] = lang.MangleFileName
-	f["toPackagePath"] = func(name string) string {
-		return filepath.FromSlash(lang.ManglePackagePath(name, ""))
-	}
-	f["toPackage"] = func(name string) string {
-		return lang.ManglePackagePath(name, "")
-	}
-	f["toPackageName"] = func(name string) string {
-		return lang.ManglePackageName(name, "")
-	}
-	f["arrayInitializer"] = lang.ArrayInitializer
-	f["imports"] = lang.Imports
-
-	// Generator-type-dependent entries.
-	f["paramDocType"] = func(param GenParameter) string {
-		return resolvedDocType(param.SwaggerType, param.SwaggerFormat, param.Child)
-	}
-	f["headerDocType"] = func(header GenHeader) string {
-		return resolvedDocType(header.SwaggerType, header.SwaggerFormat, header.Child)
-	}
-	f["schemaDocType"] = func(in any) string {
-		switch schema := in.(type) {
-		case GenSchema:
-			return resolvedDocSchemaType(schema.SwaggerType, schema.SwaggerFormat, schema.Items)
-		case *GenSchema:
-			if schema == nil {
-				return ""
-			}
-			return resolvedDocSchemaType(schema.SwaggerType, schema.SwaggerFormat, schema.Items)
-		case GenDefinition:
-			return resolvedDocSchemaType(schema.SwaggerType, schema.SwaggerFormat, schema.Items)
-		case *GenDefinition:
-			if schema == nil {
-				return ""
-			}
-			return resolvedDocSchemaType(schema.SwaggerType, schema.SwaggerFormat, schema.Items)
-		default:
-			panic("dev error: schemaDocType should be called with GenSchema or GenDefinition")
-		}
-	}
-	f["schemaDocMapType"] = func(schema GenSchema) string {
-		return resolvedDocElemType("object", schema.SwaggerFormat, &schema.resolvedType)
-	}
-	f["docCollectionFormat"] = resolvedDocCollectionFormat
-	f["path"] = errorPath
-
-	// CLI command helpers that depend on generator types.
-	f["cmdName"] = func(in any) (string, error) {
-		op, isOperation := in.(GenOperation)
-		if !isOperation {
-			ptr, ok := in.(*GenOperation)
-			if !ok {
-				return "", fmt.Errorf("cmdName should be called on a GenOperation, but got: %T", in)
-			}
-			op = *ptr
-		}
-		name := "Operation" + pascalize(op.Package) + pascalize(op.Name) + "Cmd"
-
-		return name, nil
-	}
-	f["cmdGroupName"] = func(in any) (string, error) {
-		opGroup, ok := in.(GenOperationGroup)
-		if !ok {
-			return "", fmt.Errorf("cmdGroupName should be called on a GenOperationGroup, but got: %T", in)
-		}
-		name := "GroupOfOperations" + pascalize(opGroup.Name) + "Cmd"
-
-		return name, nil
-	}
-
-	// assert is used to inject into templates and check for inconsistent/invalid data.
-	f["assert"] = func(msg string, assertion bool) (string, error) {
-		if !assertion {
-			return "", fmt.Errorf("%v: %w", msg, errInternal)
-		}
-
-		return "", nil
-	}
-
-	return f
 }
